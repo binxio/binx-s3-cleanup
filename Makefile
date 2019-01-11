ed3bb3476572b96b9c18b053030353b75d648053
kkessels <kevinkessels@binx.io>
2019-01-11 17:59:41 +0100

    first working verion of tag based removal lambda

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
-.PHONY: help deploy delete create-bucket build package
+.PHONY: help deploy upload-content cleanup-content delete deployment-bucket build package
 .DEFAULT_GOAL := run
 
+## set your profile name as ENVIRONMENT
 ENVIRONMENT = "binx"
 BUCKET_NAME="binx-cleanup-lambda"
 
@@ -10,13 +11,34 @@ help:
 deploy: build package
 	@sceptre launch $(ENVIRONMENT)
 
-delete: ## delete env
+upload-content:
+	## copy some test content to the buckets
+	aws s3 cp bucket-content/index1.html s3://$(ENVIRONMENT)-s3-cleanup-bucket1/index1.html
+	aws s3 cp bucket-content/index2.html s3://$(ENVIRONMENT)-s3-cleanup-bucket2/index2.html
+	aws s3 cp bucket-content/hello.txt s3://$(ENVIRONMENT)-s3-cleanup-bucket3/hello.txt
+	aws s3 cp bucket-content/real-content.txt s3://$(ENVIRONMENT)-s3-cleanup-bucket4/real-content.txt
+	aws s3 cp bucket-content/hello.txt s3://$(ENVIRONMENT)-s3-cleanup-bucket5/hello.txt
+	aws s3 cp bucket-content/real-content.txt s3://$(ENVIRONMENT)-s3-cleanup-bucket5/real-content.txt
+
+cleanup-content:
+	## clean the test content in the buckets
+	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket1 --recursive
+	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket2 --recursive
+	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket3 --recursive
+	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket4 --recursive
+	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket5 --recursive
+
+	#cleanup deployment bucket
+	aws s3 rm s3://binx-cleanup-lambda --recursive
+
+delete:
 	@sceptre delete $(ENVIRONMENT)
 
-create-bucket:
+deployment-bucket:
 	@sceptre launch $(ENVIRONMENT)/eu/bucket.yaml
 
 build:
+	export AWS_PROFILE=$(ENVIRONMENT)
 	sam build \
 		-s lambdas/ \
 		-t sam-templates/s3cleanup.yaml \
@@ -24,6 +46,7 @@ build:
 		--use-container
 
 package:
+	export AWS_PROFILE=$(ENVIRONMENT)
 	sam package \
 		--output-template-file templates/s3cleanup.yaml \
 		--s3-bucket $(BUCKET_NAME)
--- lambdas/s3_cleanup.py
+++ lambdas/s3_cleanup.py
@@ -1,15 +1,71 @@
+from botocore.exceptions import ClientError
 import boto3
 import datetime
 
 s3_client = boto3.client('s3')
+s3_resource = boto3.resource('s3')
 now = datetime.datetime.now()
 
 
+def determine_buckets_to_remove(buckets):
+    buckets_to_remove = []
+
+    try:
+        for bucket in buckets:
+            bucket_tagging = s3_client.get_bucket_tagging(
+                Bucket=bucket['Name']
+            )
+            print(f"Bucket: {bucket['Name']} has tags: {bucket_tagging['TagSet']}")
+
+            owner_tag = {'Key': 'owner', 'Value': 'ocp'}
+            at_tag = {'Key': 'at', 'Value': 'delete'}
+            prod_tag = {'Key': 'prod', 'Value': 'delete'}
+
+            if owner_tag in bucket_tagging['TagSet']:
+                print('Owner tags found!')
+                if at_tag in bucket_tagging['TagSet'] or prod_tag in bucket_tagging['TagSet']:
+                    print(f"Adding bucket: {bucket['Name']} to removal list")
+                    buckets_to_remove.append(bucket['Name'])
+    except ClientError as e:
+        if e.response["Error"]["Code"] == 'NoSuchTagSet':
+            pass
+
+    return buckets_to_remove
+
+
+def remove_buckets(buckets_to_remove):
+    print(f'Buckets to be removed are: {buckets_to_remove}')
+
+    for bucket in buckets_to_remove:
+        print(f'Emptying bucket: {bucket}')
+        bucket = s3_resource.Bucket(bucket)
+        bucket.objects.all().delete()
+
+        print(f'Deleting bucket {bucket}')
+        bucket.delete()
+
+    print(f'Done removing buckets')
+
+
 def handler(event, context):
-    buckets = s3_client.list_buckets()
+    try:
+        buckets = s3_client.list_buckets()['Buckets']
 
-    print(f'All buckets ====> {buckets}')
+        print(f'All buckets ====> {buckets}')
 
-    return {
-        'statusCode': 200,
-    }
+        buckets_to_remove = determine_buckets_to_remove(buckets)
+        remove_buckets(buckets_to_remove)
+
+        return {
+            'statusCode': 200,
+        }
+    except ClientError as e:
+        print(f'Unexpected client error: {e}')
+        return {
+            'statusCode': 400,
+        }
+    except Exception as e:
+        print(f'Something went horribly wrong: {e}')
+        return {
+            'statusCode': 400,
+        }
