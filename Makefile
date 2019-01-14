c5db346727894fa65e3f13d822b1f613f36bbcba
kkessels <kevinkessels@binx.io>
2019-01-14 15:18:21 +0100

    Reworked lambda to accept tags via schedule event input

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: help deploy upload-content cleanup-content delete deployment-bucket build package
+.PHONY: help launch deploy redeploy remake-buckets upload-content cleanup-test-content cleanup-deployment-content deployment-bucket build package delete
 .DEFAULT_GOAL := run
 
 ## set your profile name as ENVIRONMENT
@@ -8,8 +8,16 @@ BUCKET_NAME="binx-cleanup-lambda"
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
+launch: deployment-bucket deploy upload-content
+
 deploy: build package
 	@sceptre launch $(ENVIRONMENT)
+
+redeploy: cleanup-test-content remake-buckets upload-content
+
+remake-buckets:
+	@sceptre delete $(ENVIRONMENT)/eu/ocp-buckets.yaml
+	@sceptre launch $(ENVIRONMENT)/eu/ocp-buckets.yaml
 
 upload-content:
 	## copy some test content to the buckets
@@ -20,19 +28,15 @@ upload-content:
 	aws s3 cp bucket-content/hello.txt s3://$(ENVIRONMENT)-s3-cleanup-bucket5/hello.txt
 	aws s3 cp bucket-content/real-content.txt s3://$(ENVIRONMENT)-s3-cleanup-bucket5/real-content.txt
 
-cleanup-content:
-	## clean the test content in the buckets
-	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket1 --recursive
-	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket2 --recursive
-	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket3 --recursive
-	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket4 --recursive
-	aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket5 --recursive
+cleanup-test-content:
+	-aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket1 --recursive
+	-aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket2 --recursive
+	-aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket3 --recursive
+	-aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket4 --recursive
+	-aws s3 rm s3://$(ENVIRONMENT)-s3-cleanup-bucket5 --recursive
 
-	#cleanup deployment bucket
+cleanup-deployment-content:
 	aws s3 rm s3://binx-cleanup-lambda --recursive
-
-delete:
-	@sceptre delete $(ENVIRONMENT)
 
 deployment-bucket:
 	@sceptre launch $(ENVIRONMENT)/eu/bucket.yaml
@@ -50,3 +54,6 @@ package:
 	sam package \
 		--output-template-file templates/s3cleanup.yaml \
 		--s3-bucket $(BUCKET_NAME)
+
+delete: cleanup-test-content cleanup-deployment-content
+	@sceptre delete $(ENVIRONMENT)
--- lambdas/s3_cleanup.py
+++ lambdas/s3_cleanup.py
@@ -7,25 +7,20 @@ s3_resource = boto3.resource('s3')
 now = datetime.datetime.now()
 
 
-def determine_buckets_to_remove(buckets):
-    buckets_to_remove = []
+def determine_buckets_to_remove(buckets, tags, context):
+    buckets_to_remove = set()
 
     try:
         for bucket in buckets:
             bucket_tagging = s3_client.get_bucket_tagging(
                 Bucket=bucket['Name']
             )
-            print(f"Bucket: {bucket['Name']} has tags: {bucket_tagging['TagSet']}")
+            context.log(f"Bucket: {bucket['Name']} has tags: {bucket_tagging['TagSet']}")
 
-            owner_tag = {'Key': 'owner', 'Value': 'ocp'}
-            at_tag = {'Key': 'at', 'Value': 'delete'}
-            prod_tag = {'Key': 'prod', 'Value': 'delete'}
+            for tagset in tags:
+                if all([tag in bucket_tagging['TagSet'] for tag in tagset]):
+                    buckets_to_remove.add(bucket['Name'])
 
-            if owner_tag in bucket_tagging['TagSet']:
-                print('Owner tags found!')
-                if at_tag in bucket_tagging['TagSet'] or prod_tag in bucket_tagging['TagSet']:
-                    print(f"Adding bucket: {bucket['Name']} to removal list")
-                    buckets_to_remove.append(bucket['Name'])
     except ClientError as e:
         if e.response["Error"]["Code"] == 'NoSuchTagSet':
             pass
@@ -33,39 +28,45 @@ def determine_buckets_to_remove(buckets):
     return buckets_to_remove
 
 
-def remove_buckets(buckets_to_remove):
+def remove_buckets(buckets_to_remove, context):
     print(f'Buckets to be removed are: {buckets_to_remove}')
 
     for bucket in buckets_to_remove:
-        print(f'Emptying bucket: {bucket}')
+        context.log(f'Emptying bucket: {bucket}')
         bucket = s3_resource.Bucket(bucket)
-        bucket.objects.all().delete()
+        # bucket.objects.all().delete()
 
-        print(f'Deleting bucket {bucket}')
-        bucket.delete()
+        context.log(f'Deleting bucket {bucket}')
+        # bucket.delete()
 
-    print(f'Done removing buckets')
+
+def prepare_tags(event, context):
+    tags = []
+
+    for tags_dict in event:
+        tagset = []
+        for k, v in tags_dict.items():
+            tagset.append({'Key': f'{k}', 'Value': f'{v}'})
+        tags.append(tagset)
+
+    context.log(f'The tags are ==> {tags}')
+    return tags
 
 
 def handler(event, context):
     try:
+        context.log(f'The event is ==> {event}')
         buckets = s3_client.list_buckets()['Buckets']
 
-        print(f'All buckets ====> {buckets}')
-
-        buckets_to_remove = determine_buckets_to_remove(buckets)
-        remove_buckets(buckets_to_remove)
+        tags = prepare_tags(event, context)
+        buckets_to_remove = determine_buckets_to_remove(buckets, tags, context)
+        remove_buckets(buckets_to_remove, context)
 
         return {
             'statusCode': 200,
         }
     except ClientError as e:
-        print(f'Unexpected client error: {e}')
-        return {
-            'statusCode': 400,
-        }
-    except Exception as e:
-        print(f'Something went horribly wrong: {e}')
+        context.log(f'Unexpected client error: {e}')
         return {
             'statusCode': 400,
         }
